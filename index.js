2f4e2a5858e58077714c785dc36fffd7b805622c
heshuo <2393097587@qq.com>
2022-10-26 23:06:53 +0800

    5.0

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 // 1.引入 fs 模块
 const fs = require('fs')
-fs.readFile('./data/古诗.md', (err, data) => {
+// 引入 util模块
+const util = require('util')
+/* fs.readFile('./data/古诗.md', (err, data) => {
     if(err) throw err;
     console.log(data.toString());
-})
+}) */
 
-// 2. 使用Promise封装
-const p = new Promise((resolve, reject) => {
-    fs.readFile("./data/古诗.md", (err, data) => {
-        //判断如果失败
-        if(err) reject(err)
-        //如果成功
-        resolve(data.toString())
+/* // 2. 使用Promise封装
+function promise(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            //如果成功
+            resolve(data.toString())
+            //判断如果失败
+            if(err) reject(err)
+        })
     })
-})
-
-p.then((value) => {
+    
+}
+promise('./data/古诗.md').then((value) => {
     console.log(value);
 }, (reason) => {
     console.log('读取失败!', reason);
-})
+}) */
 
+// 使用util模块
+let findMe = util.promisify(fs.readFile)
+
+findMe('./data/古诗.md').then(value => {
+    console.log(value.toString());
+})
